3e43ac535d364bfa5dece20b10b441bd17104809
Sapporo Ningyo <103877981+NewSapporoNingyo@users.noreply.github.com>
2025-10-06 20:48:44 +0900

    add bushes

--- resources/tools/bush.py
+++ resources/tools/bush.py
@@ -1,14 +1,14 @@
 import random
 # 自动种草机
 # 参数设置
-x_start = 7      # 起始X
-x_end = 51       # 终止X
+x_start = 1      # 起始X
+x_end = 15       # 终止X
 x_step = 2       # 每组间隔
-y_min, y_max = -2.2, -1.6  # 草丛高度上下浮动范围
+y_min, y_max = -2.0, -1.4  # 草丛高度上下浮动范围
 z_step = 5        # 前后间隔
 bush_per_group = 5  # 每组贴图数量
 repeat_prefix = "bushR"    # 前缀（bushL/bushR）
-group_start_index = 2      # 从哪一组编号开始（比如已有到bushL3，就从bushL4开始）
+group_start_index = -1      # 从哪一组编号开始（比如已有到bushL3，就从bushL4开始）
 
 # 自动生成
 group_index = group_start_index + 1
